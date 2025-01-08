33c669cd64554df83401191183bf1c0ba2910bd9
lizhaowen <690591397@qq.com>
2025-01-08 11:22:24 +0800

    rename

--- src/views/folder/Index.vue
+++ src/views/folder/Index.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
-import { computed, onMounted, ref, type ComputedRef, type Ref } from 'vue';
-import { createDir, deleteRes } from '@/ctrls/index'
+import { computed, nextTick, onMounted, reactive, ref, type ComputedRef, type Ref } from 'vue';
+import { createDir, deleteRes, renameDir, renameFile } from '@/ctrls/index'
 import { MetadataTypeDefaultValue, type MetadataType } from '@/types';
 import Options from './Options.vue';
 import { useMetadatasStore } from '@/stores/metadatas'
-import { getCurrentFolder } from './utils'
+import { DOPTION_VALUES, getCurrentFolder } from './utils'
 import { handleCreateDir, handleOptionSelected } from './folder'
+import { PATH_TYPE } from '@/const';
+import { Message } from '@arco-design/web-vue';
 
 const metadataStore = useMetadatasStore();
 
@@ -50,6 +52,46 @@ const handleCreateDirButtonClick = () => {
   }
 }
 
+const inputRef = ref<HTMLInputElement | null>(null);
+const newName = ref('')
+const renameRecord = ref<MetadataType | undefined>(undefined)
+const handleRename = async (message: string | number | Record<string, any> | undefined, record: MetadataType) => {
+  if (message === DOPTION_VALUES.Rename) {
+    renameRecord.value = record;
+    newName.value = record.name;
+    await nextTick();
+    if (inputRef.value) {
+      inputRef.value.focus();
+    }
+  }
+}
+const resetRenameStatus = () => {
+  renameRecord.value = undefined;
+  inputRef.value = null;
+  newName.value = '';
+}
+const handleRenameBlurCheck = async () => {
+  if (renameRecord.value?.name === newName.value) {
+    return resetRenameStatus();
+  }
+
+  const record = renameRecord.value;
+  if (!record) return;
+  const params = {
+    target: record?.path,
+    name: newName.value,
+  }
+  const fn = PATH_TYPE.DIR === record.type ? renameDir : renameFile;
+  const res = await fn(params);
+  const result = await res.json();
+  if (res.status !== 200 || result.code !== 0) {
+    Message.error(result.message);
+    return;
+  }
+  setTimeout(() => {
+    resetRenameStatus();
+  }, 100);
+}
 </script>
 
 <template>
@@ -75,9 +117,13 @@ const handleCreateDirButtonClick = () => {
         <template #columns>
           <a-table-column title="Name" data-index="mtime">
             <template #cell="{ record }">
-              <span class="home-view-table-file-name-class" v-on:click="cellClick(record)">
+              <span v-show="record.ino !== renameRecord?.ino" class="home-view-table-file-name-class"
+                v-on:click="cellClick(record)">
                 {{ record.name }}
               </span>
+              <a-input v-if="record.ino === renameRecord?.ino" ref="inputRef" :style="{ width: '320px' }"
+                @blur="handleRenameBlurCheck" @press-enter="handleRenameBlurCheck" v-model="newName"
+                placeholder="Please enter new name" allow-clear />
             </template>
           </a-table-column>
           <a-table-column title="Size" data-index="size" :width="180">
@@ -90,6 +136,7 @@ const handleCreateDirButtonClick = () => {
               <div style="display: flex; justify-content: space-between; align-items: center;">
                 <div>{{ record.mtime }}</div>
                 <Options @selected="(k) => {
+  handleRename(k, record)
                   handleOptionSelected(k, record)
                 }" :record="record" />
               </div>
