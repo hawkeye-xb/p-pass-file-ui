5eaff0c94af56c3650a706d398023a8d05ec66e4
lizhaowen <690591397@qq.com>
2025-02-08 15:38:40 +0800

    多语言处理

--- src/views/usage/folder/Footer.vue
+++ src/views/usage/folder/Footer.vue
@@ -1,18 +1,21 @@
 <script setup lang="ts">
 import { DOPTION_VALUES } from './utils';
+import { useI18n } from 'vue-i18n';
 
+const { t } = useI18n();
 const emit = defineEmits<{
 	(e: 'selected', message: DOPTION_VALUES): void
 }>();
 </script>
+
 <template>
 	<div class="folder-view-footer">
 		<a-space>
-			<!-- <a-button @click="emit('selected', DOPTION_VALUES.Share)">Share</a-button> -->
-			<a-button @click="emit('selected', DOPTION_VALUES.Download)">Download</a-button>
-			<!-- <a-button @click="emit('selected', DOPTION_VALUES.Copy)">Copy</a-button> -->
-			<a-button @click="emit('selected', DOPTION_VALUES.MoveTo)">Move To</a-button>
-			<a-button status="danger" @click="emit('selected', DOPTION_VALUES.Delete)">Delete</a-button>
+			<!-- <a-button @click="emit('selected', DOPTION_VALUES.Share)">{{ t('folder.options.share') }}</a-button> -->
+			<a-button @click="emit('selected', DOPTION_VALUES.Download)">{{ t('folder.options.download') }}</a-button>
+			<!-- <a-button @click="emit('selected', DOPTION_VALUES.Copy)">{{ t('folder.options.copy') }}</a-button> -->
+			<a-button @click="emit('selected', DOPTION_VALUES.MoveTo)">{{ t('folder.options.moveTo') }}</a-button>
+			<a-button status="danger" @click="emit('selected', DOPTION_VALUES.Delete)">{{ t('folder.options.delete') }}</a-button>
 		</a-space>
 	</div>
 </template>
